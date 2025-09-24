42028fe4d543f1dfa62bed8f40c323fd8ef3aea6
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2025-09-24 14:18:28 +0100

    feat: add splash screen

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -7,6 +7,7 @@ import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persi
 import { Persister } from '@tanstack/react-query-persist-client';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootComponent } from './components/root/RootComponent';
+import BootSplash from "react-native-bootsplash";
 
 function App() {
   const [bootstrapped, setBootstrapped] = useState(false);
@@ -23,6 +24,11 @@ function App() {
         });
         setPersister(reactQueryPersistor);
         setBootstrapped(true);
+
+        //we defer the hiding so the initial layout can happen
+        setTimeout(() => {
+          BootSplash.hide({ fade: true });
+        }, 200)
       });
     }
   }, [bootstrapped, bootstrap, kvService]);
