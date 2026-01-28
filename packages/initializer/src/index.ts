a30ad2560f4bc0a4d153a2a9d4a8678dcdf5b606
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2026-01-28 17:25:51 +0000

    chore: formatting/copyright

--- packages/initializer/src/index.ts
+++ packages/initializer/src/index.ts
@@ -9,6 +9,7 @@
  See the License for the specific language governing permissions and
  limitations under the License
  */
+
 export const name = '@perawallet/wallet-core-initializer'
 
 export * from './initialize'
