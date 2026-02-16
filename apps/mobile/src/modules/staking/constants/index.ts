71ba64005db0d1dc5b98e39cb91c15a269dce9ce
Yasin Çalışkan <yasincaliskandev@gmail.com>
2026-02-16 14:17:37 +0300

    feat(staking): fetch project details from firebase remote config

--- apps/mobile/src/modules/staking/constants/index.ts
+++ apps/mobile/src/modules/staking/constants/index.ts
@@ -10,7 +10,7 @@
  limitations under the License
  */
 
-import type { StakingProjectInfo, StakingType } from '../models'
+import type { StakingType } from '../models'
 
 export const STAKING_TYPE_LABELS: Record<StakingType, string> = {
     liquid: 'Liquid Staking',
@@ -30,87 +30,6 @@ export const STAKING_TYPE_DESCRIPTIONS: Record<StakingType, string> = {
     delegated:
         'Delegated staking involves using a third-party to run a node on your behalf while your Algo remains in your wallet. This is useful for users who want to secure the network and collect rewards without running their own node.',
 }
-
-export const STAKING_PROJECTS: StakingProjectInfo[] = [
-    {
-        id: 'folks',
-        title: 'Folks Finance',
-        description:
-            'Stake your Algo on Folks to collect rewards and receive xALGO',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/xAlgo%20Liquid%20Staking.png?width=80&height=80&name=xAlgo%20Liquid%20Staking.png',
-        link: 'https://app.folks.finance/liquid-staking?ref=pera',
-        type: 'liquid',
-    },
-    {
-        id: 'tinyman',
-        title: 'Tinyman',
-        description:
-            'Stake your Algo on Tinyman to collect rewards and receive tALGO',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/tALGO@2x.png?width=80&height=80&name=tALGO@2x.png',
-        link: 'https://app.tinyman.org/liquid-stake/stake',
-        type: 'liquid',
-    },
-    {
-        id: 'compx',
-        title: 'CompX',
-        description:
-            'Stake your Algo on CompX to collect rewards and receive cALGO',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/cAlgo.png?width=80&height=80&name=cAlgo.png',
-        link: 'https://app.compx.io/staking-pools-pera',
-        type: 'liquid',
-    },
-    {
-        id: 'pact',
-        title: 'Pact',
-        description:
-            'On Pact, eligible Algo-paired liquidity pools (LPs) will automatically participate in consensus',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/Pact_BrandKit_Logomark_Blue-1.png?width=80&height=80&name=Pact_BrandKit_Logomark_Blue-1.png',
-        link: 'https://app.pact.fi/stake',
-        type: 'pools',
-    },
-    {
-        id: 'reti',
-        title: 'Réti',
-        description: 'Pool your Algo with others to participate in consensus',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/R%C3%A9ti-Pooling_og-image@2x_icon.png?width=80&height=80&name=R%C3%A9ti-Pooling_og-image@2x_icon.png',
-        link: 'https://reti.nodely.io/',
-        type: 'pools',
-    },
-    {
-        id: 'valar',
-        title: 'Valar',
-        description:
-            'Stake your Algo to a network of node runners without it leaving your wallet',
-        logoUrl:
-            'https://algorand.co/hs-fs/hubfs/Valar2.png?width=80&height=80&name=Valar2.png',
-        link: 'https://stake.valar.solutions/',
-        type: 'delegated',
-    },
-    {
-        id: 'messina',
-        title: 'Messina',
-        description:
-            'Stake your Algo on Messina to collect rewards and receive mALGO',
-        logoUrl:
-            'https://pbs.twimg.com/profile_images/1798989048436166656/-boBIisR_400x400.jpg',
-        link: 'https://messina.one/liquid-staking',
-        type: 'liquid',
-    },
-    {
-        id: 'myth',
-        title: 'Myth Finance',
-        description:
-            'Stake your Algo on Myth to collect rewards and receive dualSTAKE Tokens',
-        logoUrl: 'https://myth.finance/favicon.ico',
-        link: 'https://myth.finance/dualSTAKE',
-        type: 'liquid',
-    },
-]
 
 export const STAKING_DISCLAIMER_STORAGE_KEY = 'pera.staking.disclaimerAccepted'
 
