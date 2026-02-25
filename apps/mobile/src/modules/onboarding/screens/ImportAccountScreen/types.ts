c115e56a70d5ee8de5048ccacab053d9a0c1e05c
Fred Souza <1396615+fmsouza@users.noreply.github.com>
2026-02-25 13:37:06 +0000

    fix(mobile): fixing mnemonics screen behavior [PERA-3698] (#134)

---
+++ apps/mobile/src/modules/onboarding/screens/ImportAccountScreen/types.ts
@@ -0,0 +1,40 @@
+/*
+ Copyright 2022-2025 Pera Wallet, LDA
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License
+ */
+
+import type { PWInputRef } from '@components/core'
+
+export type UseImportAccountScreenResult = {
+    words: string[]
+    focused: number
+    setFocused: (index: number) => void
+    canImport: boolean
+    processing: boolean
+    updateWord: (word: string, index: number) => void
+    handleWordChange: (word: string, index: number) => void
+    handleImportAccount: () => void
+    mnemonicLength: number
+    t: (key: string) => string
+    isKeyboardVisible: boolean
+    keyboardHeight: number
+    isSupportOptionsVisible: boolean
+    handleOpenSupportOptions: () => void
+    handleCloseSupportOptions: () => void
+    handlePastePassphrase: () => void
+    handleScanQRCode: () => void
+    handleLearnMore: () => void
+    isQRScannerVisible: boolean
+    handleCloseQRScanner: () => void
+    handleQRScannerSuccess: (url: string) => void
+    suggestions: string[]
+    handleSelectSuggestion: (word: string) => void
+    refCallbacks: ((ref: PWInputRef | null) => void)[]
+}
