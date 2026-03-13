8b9529fedccef4f0f11f8ac83324bcba928f993d
Fred Souza <1396615+fmsouza@users.noreply.github.com>
2026-03-13 12:02:35 +0000

    fix: fixing autocollapse on the account asset list [PERA-3693] (#196)

--- apps/mobile/src/modules/accounts/components/AccountAssetList/AccountAssetList.tsx
+++ apps/mobile/src/modules/accounts/components/AccountAssetList/AccountAssetList.tsx
@@ -127,8 +127,8 @@ export const AccountAssetList = ({
                     contentContainerStyle={styles.rootContainer}
                     ListHeaderComponent={
                         <PWView style={styles.headerContainer}>
-                            {header}
                             <ExpandablePanel isExpanded={headerState.isOpen}>
+                                {header}
                                 <PWView style={styles.titleBar}>
                                     <PWText
                                         style={styles.title}
@@ -161,6 +161,7 @@ export const AccountAssetList = ({
                             </ExpandablePanel>
                             <SearchInput
                                 onFocus={headerState.close}
+                                onBlur={headerState.open}
                                 placeholder={t(
                                     'account_details.assets.search_placeholder',
                                 )}
