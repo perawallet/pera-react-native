49d3e5c897e5d7ea96e968992428189a6f987e51
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2026-03-17 19:29:49 +0000

    fix: height of buttons with title vs without [PERA-3886] (#212)

--- apps/mobile/src/components/core/PWButton/styles.ts
+++ apps/mobile/src/components/core/PWButton/styles.ts
@@ -98,7 +98,7 @@ export const useStyles = makeStyles((theme, props: PWButtonProps) => {
         titleStyle: {
             fontFamily: getFontFamily(500),
             fontSize: 15,
-            lineHeight: 24,
+            lineHeight: 15,
             flexWrap: 'nowrap',
             textAlign: 'center',
             verticalAlign: 'middle',
