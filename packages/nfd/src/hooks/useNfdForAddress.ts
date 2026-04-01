1fe3dea8d5948eb35c067ea7d4622dfa97efde8c
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2026-04-01 15:18:55 +0100

    feat: implement NFD lookups [PERA-3631]

---
+++ packages/nfd/src/hooks/useNfdForAddress.ts
@@ -0,0 +1,53 @@
+/*
+ Copyright 2022-2025 Pera Wallet, LDA
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License
+ */
+
+import { useMemo } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import {
+    useNetwork,
+    isValidAlgorandAddress,
+} from '@perawallet/wallet-core-blockchain'
+import { fetchNfdNamesForAddress } from '../api'
+import { nfdQueryKeys } from './querykeys'
+
+const NFD_STALE_TIME = 5 * 60 * 1000
+const NFD_GC_TIME = 30 * 60 * 1000
+
+type UseNfdForAddressResult = {
+    nfdName: string | undefined
+    isResolving: boolean
+}
+
+export const useNfdForAddress = (
+    address: string,
+    options?: { enabled?: boolean },
+): UseNfdForAddressResult => {
+    const { network } = useNetwork()
+    const enabled =
+        (options?.enabled ?? true) && isValidAlgorandAddress(address)
+
+    const { data, isFetching } = useQuery({
+        queryKey: nfdQueryKeys.forAddress(address, network),
+        queryFn: ({ signal }) =>
+            fetchNfdNamesForAddress({ address, network, signal }),
+        staleTime: NFD_STALE_TIME,
+        gcTime: NFD_GC_TIME,
+        enabled,
+    })
+
+    const nfdName = useMemo(() => data?.at(0)?.name, [data])
+
+    return {
+        nfdName,
+        isResolving: isFetching,
+    }
+}
