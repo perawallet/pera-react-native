ccbdd0ec6cc428362360224d8e7dbc02df011c1d
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2026-07-17 12:51:20 +0100

    chore(extension): bump copyright headers to 2026 via formatter

--- apps/extension/src/content/__tests__/relay-isolated.test.ts
+++ apps/extension/src/content/__tests__/relay-isolated.test.ts
@@ -1,5 +1,5 @@
 /*
- Copyright 2022-2025 Pera Wallet, LDA
+ Copyright 2022-2026 Pera Wallet, LDA
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,6 +9,7 @@
  See the License for the specific language governing permissions and
  limitations under the License
  */
+
 // @vitest-environment jsdom
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { CHANNEL_HANDSHAKE_EVENT, CHANNEL_RELAY_READY_EVENT } from '../channel'
