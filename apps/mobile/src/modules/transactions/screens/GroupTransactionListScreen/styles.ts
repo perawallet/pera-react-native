c493e57df1f1d9054e6bbca1e3812ce1f0b15a1b
Yasin <6695727+yasin-ce@users.noreply.github.com>
2026-07-21 11:29:02 +0300

    fix(transactions): render swap transaction group details [PERA-4658]

--- apps/mobile/src/modules/transactions/screens/GroupTransactionListScreen/styles.ts
+++ apps/mobile/src/modules/transactions/screens/GroupTransactionListScreen/styles.ts
@@ -13,12 +13,6 @@
 import { makeStyles } from '@rneui/themed'
 
 export const useStyles = makeStyles(theme => ({
-    container: {
-        flex: 1,
-    },
-    contentContainer: {
-        paddingHorizontal: theme.spacing.xl,
-    },
     itemSeparator: {
         height: theme.spacing.md,
     },
