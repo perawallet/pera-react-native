e4ffdb5a53213db3893e3cf054ef7a9b1a29c435
Will Beaumont <4557711+wjbeau@users.noreply.github.com>
2026-08-29 09:16:42 +0100

    fix: remove some top padding

--- apps/mobile/src/modules/swap/screens/SwapScreen/styles.ts
+++ apps/mobile/src/modules/swap/screens/SwapScreen/styles.ts
@@ -18,7 +18,7 @@ export const useStyles = makeStyles(theme => ({
         width: '100%',
         minWidth: 0,
         overflow: 'hidden',
-        marginTop: theme.spacing.lg,
+        marginTop: theme.spacing.sm,
     },
     toolbar: {
         flexShrink: 0,
